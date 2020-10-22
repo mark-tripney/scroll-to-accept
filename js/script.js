eff5cff79fa390b668f44aa2e4bf892ac83b05fc
Mark Tripney <mrktrpny@gmail.com>
2020-10-22 15:47:28 +0200

    Add observer callback

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const terms = document.querySelector('.terms-and-conditions');
-
-terms.addEventListener('scroll', function (e) {
-  console.log(e);
+const button = document.querySelector('.accept');
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
 });
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // Now, stop observing
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+
+ob.observe(terms.lastElementChild);
